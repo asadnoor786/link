700fd5b5fd84d06e015f4adad016f55cc76623c0
asadnoor786 <95999022+asadnoor786@users.noreply.github.com>
2022-02-16 13:12:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 80;
 app.listen(port, () => {
     console.log("Server Has Started!");
 });
